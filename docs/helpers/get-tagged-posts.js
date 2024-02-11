e177670afc60b8081ead6aad567533c8ffbedc57
Laurence Bugasu Ininda <laudebugs@gmail.com>
2024-02-10 23:02:15 -0500

    format

--- docs/helpers/get-tagged-posts.js
+++ docs/helpers/get-tagged-posts.js
@@ -6,10 +6,10 @@ import path from 'path'
 export const getImageForPost = (slug) => {
     const imagesPath = 'docs/public/assets/post-covers'
     const images = readdirSync(imagesPath)
-    const image = images.find(_image => _image.includes(slug))
+    const image = images.find((_image) => _image.includes(slug))
     return image ?? ''
-  }
-  
+}
+
 /**
  * Get all posts based on tags
  * Sorted in order of most popular tag and date
@@ -19,101 +19,114 @@ export const getAllPosts = () => {
     const yearDirectories = readdirSync(postsDirectory)
 
     const allPostsData = yearDirectories
-        .map(year => {
+        .map((year) => {
             const yearDirectory = path.join(postsDirectory, year)
             const postFiles = readdirSync(yearDirectory)
             const posts = postFiles
-            .filter(filename => /^(?!_).+.mdx/gm.test(filename))
-            .map(filename => {
-                const fullPath = path.join(`${yearDirectory}/${filename}`)
-                const post = readFileSync(fullPath, 'utf-8')
-                const { content, data } = matter(post)
-                return {
-                    content,
-                    frontMatter: data,
-                    image: getImageForPost(data.slug)
-                }
-            }).flat()
-            .sort((a, b) => compareDesc(parseISO(a.frontMatter.publishedOn), parseISO(b.frontMatter.publishedOn)))
+                .filter((filename) => /^(?!_).+.mdx/gm.test(filename))
+                .map((filename) => {
+                    const fullPath = path.join(`${yearDirectory}/${filename}`)
+                    const post = readFileSync(fullPath, 'utf-8')
+                    const { content, data } = matter(post)
+                    return {
+                        content,
+                        frontMatter: data,
+                        image: getImageForPost(data.slug),
+                    }
+                })
+                .flat()
+                .sort((a, b) => compareDesc(parseISO(a.frontMatter.publishedOn), parseISO(b.frontMatter.publishedOn)))
             return posts
-        }).reverse().flat()
+        })
+        .reverse()
+        .flat()
     return allPostsData
 }
+
 /**
  * Get all posts based on tags
  * Sorted in order of most popular tag and date
  * @returns {Record<string, {count: number, posts: {title: string, slug: string, date: string}[], }>}
  */
 export const getPostsByTags = () => {
-
     const allPostsData = getAllPosts()
 
-    const tagBasedPosts = allPostsData
-        .reduce((tagCollection, post) => {
-            const tags = post.frontMatter.tags
-            if(tags.length === 0) return tagCollection
-            const mainTag = tags[0]
-                if (tagCollection[mainTag]) {
-                    tagCollection[mainTag]['count'] = tagCollection[mainTag]['count'] + 1
-                    tagCollection[mainTag]['posts'].push({
+    const tagBasedPosts = allPostsData.reduce((tagCollection, post) => {
+        const tags = post.frontMatter.tags
+        if (tags.length === 0) return tagCollection
+
+        const mainTag = tags[0]
+        if (tagCollection[mainTag]) {
+            tagCollection[mainTag]['count'] = tagCollection[mainTag]['count'] + 1
+            tagCollection[mainTag]['posts'].push({
+                title: post.frontMatter.title,
+                slug: parseISO(post.frontMatter.publishedOn).getFullYear() + '/' + post.frontMatter.slug,
+                date: post.frontMatter.publishedOn,
+            })
+        } else {
+            tagCollection[mainTag] = {
+                count: 1,
+                posts: [
+                    {
                         title: post.frontMatter.title,
                         slug: parseISO(post.frontMatter.publishedOn).getFullYear() + '/' + post.frontMatter.slug,
-                        date: post.frontMatter.publishedOn
-                    })
-                } else {
-                    tagCollection[mainTag] = {
-                        count: 1,
-                        posts: [{
-                            title: post.frontMatter.title,
-                            slug: parseISO(post.frontMatter.publishedOn).getFullYear() + '/' + post.frontMatter.slug,
-                            date: post.frontMatter.publishedOn
-                        }]
-                    }
-                }
+                        date: post.frontMatter.publishedOn,
+                    },
+                ],
+            }
+        }
 
-            return tagCollection
-        }, {})
+        return tagCollection
+    }, {})
+
     return tagBasedPosts
 }
 
 /**
- * 
+ *
  * @returns {Array<{text: string, collapsed: boolean, items: {text: string, link: string}[]}>}
  */
 export const getPostsByYear = () => {
     const allPostsData = getAllPosts()
+
     const yearBasedPosts = allPostsData.reduce((yearCollection, post) => {
         const year = parseISO(post.frontMatter.publishedOn).getFullYear()
+
         if (yearCollection[year]) {
             yearCollection[year].push({
                 title: post.frontMatter.title,
                 slug: parseISO(post.frontMatter.publishedOn).getFullYear() + '/' + post.frontMatter.slug,
-                date: post.frontMatter.publishedOn
+                date: post.frontMatter.publishedOn,
             })
         } else {
-            yearCollection[year] = [{
-                title: post.frontMatter.title,
-                slug: parseISO(post.frontMatter.publishedOn).getFullYear() + '/' + post.frontMatter.slug,
-                date: post.frontMatter.publishedOn
-            }]
+            yearCollection[year] = [
+                {
+                    title: post.frontMatter.title,
+                    slug: parseISO(post.frontMatter.publishedOn).getFullYear() + '/' + post.frontMatter.slug,
+                    date: post.frontMatter.publishedOn,
+                },
+            ]
         }
         return yearCollection
-    }, {} )
-    const _yearBasedPosts = Object.entries(yearBasedPosts).sort((a, b) => parseInt(b[0]) - parseInt(a[0]))
-    .map(([year, posts], index) => {
-        return {
-            text: year,
-            collapsed: index > 0 ? true : false,
-            items: posts.sort(
-                (a, b) => compareAsc(parseISO(a.date), parseISO(b.date))
-            ).reverse().map(post => {
-                return {
-                    text: post.title,
-                    link: `/posts/${post.slug}`
-                }
-            })
-        }
-    })
+    }, {})
+
+    const _yearBasedPosts = Object.entries(yearBasedPosts)
+        .sort((a, b) => parseInt(b[0]) - parseInt(a[0]))
+        .map(([year, posts], index) => {
+            return {
+                text: year,
+                collapsed: index > 0,
+                items: posts
+                    .sort((a, b) => compareAsc(parseISO(a.date), parseISO(b.date)))
+                    .reverse()
+                    .map((post) => {
+                        return {
+                            text: post.title,
+                            link: `/posts/${post.slug}`,
+                        }
+                    }),
+            }
+        })
     return _yearBasedPosts
 }
 
@@ -121,21 +134,21 @@ export const formatTagsForSideNav = () => {
     const tagBasedPosts = getPostsByTags()
     const tags = Object.entries(tagBasedPosts)
         .sort((a, b) => b[1].count - a[1].count)
-        .map(([tag, posts]) => tag)
+        .map(([tag]) => tag)
 
     return tags.map((key, index) => {
         return {
             text: `${key} (${tagBasedPosts[key].count})`,
-            collapsed: index > 0 ? true : false,
-            items: tagBasedPosts[key].posts.sort(
-                (a, b) => compareAsc(parseISO(a.date), parseISO(b.date))
-            ).reverse()
-            .map((post) => {
-                return {
-                    text: post.title,
-                    link: `/posts/${post.slug}`
-                }
-            })
+            collapsed: index > 0,
+            items: tagBasedPosts[key].posts
+                .sort((a, b) => compareAsc(parseISO(a.date), parseISO(b.date)))
+                .reverse()
+                .map((post) => {
+                    return {
+                        text: post.title,
+                        link: `/posts/${post.slug}`,
+                    }
+                }),
         }
     })
 }
